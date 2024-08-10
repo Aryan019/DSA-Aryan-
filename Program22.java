46aeb7f83c7f65fed09a3037339419cb222e1825
Aryan019 <aryan19v@gmail.com>
2024-08-10 23:16:13 +0530

    Day 10

---
+++ Program22.java
@@ -0,0 +1,53 @@
+// Sort 0 ones and two's
+
+class Solution {
+    public void sortColors(int[] nums) {
+
+        // Dutch national flag approach 
+
+        int low = 0;
+
+        int mid = 0;
+
+        int high = nums.length-1;
+
+
+        while(mid<=high){
+
+            if(nums[mid]==0){
+
+                int temp = nums[low];
+
+                nums[low] = nums[mid];
+
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            }
+
+
+            else if(nums[mid]==1){
+                mid++;
+
+            
+            }
+
+            else{
+                int temp;
+                temp = nums[high];
+
+                nums[high] = nums[mid];
+
+                nums[mid] = temp;
+                high--;
+                
+            }
+
+
+
+
+        }
+        
+    }
+}
