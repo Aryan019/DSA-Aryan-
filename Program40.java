b979349c00038e150ec7d468451e6c1f5b90ea08
Aryan019 <aryan19v@gmail.com>
2024-09-22 23:37:09 +0530

    No gap from now no day off

---
+++ Program40.java
@@ -0,0 +1,29 @@
+// Binary search starting 
+class Solution {
+    public int search(int[] nums, int target) {
+
+        // nums is the array 
+
+        //calculating mid
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            
+            int mid = (low+high)/2;
+
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+
+        return -1;
+
+        
+    }
+}
